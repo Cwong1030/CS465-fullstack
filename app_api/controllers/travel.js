e2d74eed24b94ccd60fc5889f303df9804ddd4b2
Chris Wong <wongchristopher73@yahoo.com>
2024-04-23 15:05:48 -0700

    Module 7 Updated

--- app_api/controllers/travel.js
+++ app_api/controllers/travel.js
@@ -32,46 +32,67 @@ const tripsFindCode = async (req, res) => {
 
 // POST: creates a single trip
 const tripsAddTrip = async (req, res) => {
-    getUser(req, res, (req, res) => {
-        Trip.create({
-            code: req.body.code,
-            name: req.body.name,
-            length: req.body.length,
-            start: req.body.start,
-            resort: req.body.resort,
-            perPerson: req.body.perPerson,
-            image: req.body.image,
-            description: req.body.description
-        })
-        .then(trip => {
-            return res.status(201).json(trip);
-        })
-        .catch(err => {
-            return res.status(400).json(err);
-        });
-    });
-};
+    getUser(req, res,
+        (req, res) => {
+            Trip.create({
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            },
+            (err, trip) => {
+                if (err) {
+                    return res
+                        .status(400)
+                        .json(err);
+                } else {
+                    return res
+                        .status(201)
+                        .json(trip);
+                }
+            });
+        }
+    );
+}
+
 
 // PUT: changes a single trip
 const tripsUpdateTrip = async (req, res) => {
-    Trip.findOneAndUpdate({ code: req.params.tripCode }, req.body, { new: true })
-        .then(trip => {
-            if (!trip) {
-                return res.status(404).json({ message: "Trip not found with code " + req.params.tripCode });
-            }
-            // Check user authentication after finding the trip
-            getUser(req, res, (req, res) => {
-                // Proceed with the update operation
-                return res.status(200).json(trip);
+    getUser(req, res,
+        (req, res) => {
+            Trip.findOneAndUpdate({'code': req.params.tripCode }, {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }, { new: true })
+            .then(trip => {
+                if (!trip) {
+                    return res.status(404).send({
+                        message: "Trip not found with code " + req.params.tripCode
+                    });
+                }
+                res.send(trip);
+            }).catch(err => {
+                if (err.kind === 'ObjectId') {
+                    return res.status(404).send({
+                        message: "Trip not found with code " + req.params.tripCode
+                    });
+                }
+                return res.status(500).json(err); // server error
             });
-        })
-        .catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).json({ message: "Trip not found with code " + req.params.tripCode });
-            }
-            return res.status(500).json(err);
-        });
-};
+        }
+    );
+}
+
 
 const getUser = (req, res, callback) => {
     if (req.payload && req.payload.email) {
