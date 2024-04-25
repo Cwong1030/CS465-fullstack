c7dd1852d6e81d2244ab03228cfaa4018d7c084c
Chris Wong <wongchristopher73@yahoo.com>
2024-04-25 11:48:44 -0700

    Module 7 Updated

--- app_api/controllers/authentication.js
+++ app_api/controllers/authentication.js
@@ -30,32 +30,29 @@ const register = (req, res) => {
     });
 };
 
-const login = (req, res, next) => {
-  // Check if all fields are filled
+const login = (req, res) => {
   if (!req.body.email || !req.body.password) {
     return res
-        .status(400)
-        .json({ message: 'All fields required' });
+      .status(400)
+      .json({"message": "All fields required"});
   }
-
-  // Passport authentication middleware moved into the route handler
   passport.authenticate('local', (err, user, info) => {
     if (err) {
-      console.error('Error during authentication:', err); // Log the error
       return res
-        .status(500)
-        .json({ message: 'Internal Server Error' });
+      .status(404)
+      .json(err);
     }
-    if (!user) {
-      return res
-        .status(401)
-        .json({ message: 'Authentication failed' });
-    }
+    if (user) {
       const token = user.generateJwt();
       res
         .status(200)
-        .json({ token });
-  })(req, res, next);
+        .json({token});
+    } else {
+      res
+        .status(401)
+        .json(info);
+      }
+  })(req, res);
 };
 
 module.exports = {
